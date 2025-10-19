6c2b7189d1d8e5a138a5b15501719a30cc39a6c5
hongphong177 <hong.phong@diqit.io>
2025-10-19 22:56:32 +0700

    feat: implement match diqits '\d'

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ use std::process;
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
+        input_line.contains(pattern)
+    } else if pattern == "\\d" {
+        input_line.chars().any(|c| c.is_numeric())
     } else {
         panic!("Unhandled pattern: {}", pattern)
     }
